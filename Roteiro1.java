c0e83600c507787c56bea8be99a4249f17860f43
Pedroguij0 <81244187+Pedroguij0@users.noreply.github.com>
2025-04-07 18:42:35 -0300

    Roteiro 01 - Programação de Soluções Comp.

---
+++ Roteiro1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.roteiro1;
+
+/**
+ *
+ * @author 12725111494
+ */
+public class Roteiro1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
